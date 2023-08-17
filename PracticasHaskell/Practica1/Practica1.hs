2858114b7542b1d59ee1961cc4d16fb292429ee2
Ezequiel Agustin Ibarra <ezequielibarra@vexels.com>
2023-08-17 17:59:53 -0300

    Practica1

--- PracticasHaskell/Practica1/Practica1.hs
+++ PracticasHaskell/Practica1/Practica1.hs
@@ -56,10 +56,11 @@ iguales _ _         = False
 -- 1c) Dada una dirección devuelve su siguiente, en sentido horario, y sup oniendo que no existe la siguiente dirección a Oeste.
 
 siguiente :: Dir -> Dir
+-- precondicion: la direccion no es Oeste
 siguiente Norte = Este
 siguiente Este = Sur
 siguiente Sur = Oeste
-siguiente Oeste = Norte
+siguiente _ = error "La direccion no tiene siguiente"
 
 data DiaDeSemana = Lunes | Martes | Miercoles | Jueves | Viernes | Sabado | Domingo
     deriving Show
@@ -107,6 +108,167 @@ estaEnElMedio _ = True
 
 negar :: Bool -> Bool
 negar True = False
-negar False = True
+negar _ = True
 
---3b)
+{-3b) Dados dos booleanos, si el primero es True y el segundo es False, devuelve False, sino
+devuelve True-}
+
+implica :: Bool -> Bool -> Bool
+implica True b2 = b2
+implica _ _ = True
+
+--3c) Dados dos booleanos si ambos son True devuelve True, sino devuelve False.
+
+yTambien :: Bool -> Bool -> Bool
+yTambien False _ = False
+yTambien True b2 = b2
+
+--3d) Dados dos b o oleanos si alguno de ellos es True devuelve True, sino devuelve False.
+
+oBien :: Bool -> Bool -> Bool
+oBien True _ = True
+oBien _ b2 = b2
+
+--4-------------------------------------------
+
+data Persona = P String Int 
+    deriving Show
+
+eze = P "Ezequiel" 21
+viejo = P "Viejo" 99
+
+--4.1.a) Devuelve el nombre de una persona
+
+nombre :: Persona -> String
+nombre (P n _) = n
+
+--4.1.b) Devuelve la edad de una persona
+
+edad :: Persona -> Int
+edad (P _ e) = e
+
+--4.1.c) Aumenta en uno la edad de la persona.
+
+crecer :: Persona -> Persona
+crecer (P n e) = P n (sucesor e)
+
+--4.1.d) Dados un nombre y una persona, devuelve una persona con la edad de la persona y el nuevo nombre.
+
+cambioDeNombre :: String -> Persona -> Persona
+cambioDeNombre n p = P n (edad p)
+
+--4.1.f) Dadas dos personas indica si la primera es mayor que la segunda
+
+esMayorQueLaOtra :: Persona -> Persona -> Bool
+esMayorQueLaOtra p1 p2 = edad p1 > edad p2
+
+--4.1.g) Dadas dos personas devuelve a la persona que sea mayor.
+
+laQueEsMayor :: Persona -> Persona -> Persona
+laQueEsMayor p1 p2 =
+    if edad p1 > edad p2
+        then p1
+        else p2
+
+data TipoDePokemon = Agua | Fuego | Planta
+    deriving Show
+data Pokemon = Poke TipoDePokemon Int
+    deriving Show
+
+data Entrenador = Ent String Pokemon Pokemon
+    deriving Show
+pikachu = Poke Fuego 85
+bulbasor = Poke Planta 50
+r2d2 = Poke Agua 30
+
+rokero = Ent "Rockero" pikachu r2d2
+
+pokemaniaco = Ent "Pokemaniaco" r2d2 bulbasor
+
+{-4.2.a) Dados dos Pokémon indica si el 
+primero, en base al tipo, es superior al 
+segundo. Agua supera a fuego, 
+fuego a planta y planta a agua. 
+Y cualquier otro caso es falso.
+-} 
+
+superaA :: Pokemon -> Pokemon -> Bool
+superaA p1 p2 = leGanaA (tipoDe p1) (tipoDe p2)
+
+tipoDe :: Pokemon -> TipoDePokemon
+tipoDe (Poke t _ )= t
+
+leGanaA :: TipoDePokemon -> TipoDePokemon -> Bool
+leGanaA Agua Fuego = True
+leGanaA Fuego Planta = True
+leGanaA Planta Agua = True
+leGanaA _ _ = False
+
+{-4.2.b)
+Devuelve la cantidad de Pokémon de 
+determinado tipo que posee el entrenador.-}
+
+cantidadDePokemonDe :: TipoDePokemon -> Entrenador -> Int
+cantidadDePokemonDe t (Ent _ p1 p2) =
+    unoSiCeroSino (esElMismo t (tipoDe p1)) +
+    unoSiCeroSino (esElMismo t (tipoDe p2))
+
+unoSiCeroSino :: Bool -> Int
+unoSiCeroSino True = 1
+unoSiCeroSino _ = 0
+
+esElMismo :: TipoDePokemon -> TipoDePokemon -> Bool
+esElMismo Agua Agua = True
+esElMismo Fuego Fuego = True
+esElMismo Planta Planta = True
+esElMismo _ _ = False
+
+{-4.2.c)
+Dado un par de entrenadores, 
+devuelve a sus Pokémon en una lista-}
+
+juntarPokemon :: (Entrenador, Entrenador) -> [Pokemon]
+juntarPokemon  (e1, e2) =
+    listaDePokemones (pokemonesDe e1) (pokemonesDe e2) 
+
+listaDePokemones :: [Pokemon] -> [Pokemon] -> [Pokemon]
+listaDePokemones p1s p2s = p1s ++ p2s
+
+pokemonesDe :: Entrenador -> [Pokemon]
+pokemonesDe (Ent _ p1 p2) = [p1,p2]
+
+--5----------------------------------------------
+
+--5a) Dado un elemento de algún tipo devuelve ese mismo elemento
+
+loMismo :: a -> a
+loMismo x = x
+
+--5b) Dado un elemento de algún tip o devuelve el número 7
+
+siempreSiete :: a -> Int
+siempreSiete _ = 7
+
+--5c) Dadas una tupla, invierte sus comp onentes.
+
+swap :: (a,b) -> (b, a)
+swap (x, y) = (y, x)
+
+{-  Responda la siguiente pregunta: 
+    Por qué estas funciones son polimórficas?
+
+    Porque no importa el tipo de dato que reciban,
+    siempre devuelven el mismo tipo de dato.  
+-}
+
+--6----------------------------------------------\
+
+{-6a)   
+    Dada una lista de elementos, si es 
+    vacía devuelve True, sino devuelve False.
+    Definida en Haskell como null. -}
+
+estaVacia :: [a] -> Bool
+estaVacia [] = True
+estaVacia _ = False
+
