b0afeccd5231cfdda19ac354183b65e4d4629015
Ezequiel Agustin Ibarra <ezequielibarra@vexels.com>
2023-08-23 00:21:08 -0300

    tp-2

--- tp-2/tp-2.hs
+++ tp-2/tp-2.hs
@@ -136,3 +136,56 @@ min' :: Ord a => a -> a -> a
 min' x y = if x < y
     then x
     else y
+
+--2-------------------------------------------
+
+{-2.1 Dado un número n se devuelve la 
+multiplicación de este número y todos sus 
+anteriores hasta llegar a 0. Si n es 0 
+devuelve 1. La función es parcial si n es 
+negativo. -}
+
+factorial :: Int -> Int
+factorial 0 = 1
+factorial n = n * factorial (n-1)
+
+{-2.1 Dado un número n devuelve una lista cuyos
+elementos sean los números comprendidos entre
+n y 1 (incluidos). Si el número es inferior a 
+1, devuelve la lista vacía.-}
+
+cuentaRegresiva :: Int -> [Int]
+cuentaRegresiva 0 = [] -- este caso no es necesario, pero lo pongo para que quede claro que lo tengo en cuenta 
+cuentaRegresiva n = 
+    if n < 1
+        then []
+        else n : cuentaRegresiva (n-1)
+
+{-2.2 Dado un número n y un elemento e devuelve
+una lista en la que el elemento e repite n
+veces.-}
+
+repetir :: Int -> a -> [a]
+repetir 0 _ = []
+repetir n e = e : repetir (n-1) e 
+
+{-2.3 Dados un número n y una lista xs, 
+devuelve una lista con los n primeros elementos
+de xs. Si la lista es vacía, devuelve una lista
+vacía.-}
+
+losPrimeros :: Int -> [a] -> [a]
+losPrimeros 0 _ = []
+losPrimeros _ [] = []
+losPrimeros n (x : xs) = x : losPrimeros (n-1) xs
+
+{-2.3 Dados un número n y una lista xs, 
+devuelve una lista sin los primeros n elementos
+de lista recibida. Si n es cero, devuelve la 
+lista completa.-}
+
+sinLosPrimeros :: Int -> [a] -> [a]
+-- PRECOND: n >= 0
+sinLosPrimeros 0 xs = xs
+sinLosPrimeros _ [] = []
+sinLosPrimeros n (x : xs) = sinLosPrimeros (n-1) xs
