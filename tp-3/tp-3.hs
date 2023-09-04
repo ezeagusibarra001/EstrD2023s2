a6fea805ffb94e125f8114d637ec7fc444f466bc
Ezequiel Agustin Ibarra <ezequielibarra@vexels.com>
2023-09-04 00:04:53 -0300

    tp3

--- tp-3/tp-3.hs
+++ tp-3/tp-3.hs
@@ -1,90 +1,127 @@
-data Color = Azul | Rojo deriving Show
-data Celda = Bolita Color Celda | CeldaVacia deriving Show
+data Color = Azul | Rojo
+    deriving Show
+data Celda = Bolita Color Celda | CeldaVacia
+    deriving Show
 
-celda :: Celda
 celda = Bolita Rojo (Bolita Azul (Bolita Rojo (Bolita Azul CeldaVacia)))
 
-nroBolitas :: Color -> Celda -> Int
-nroBolitas c CeldaVacia       = 0
-nroBolitas c (Bolita col cel) =  
-    if esColor c col
-      then 1 + nroBolitas c (Bolita col cel)
-      else nroBolitas c (Bolita col cel)
+{-1.1.1 Dados un color y una celda, indica la 
+cantidad de bolitas de ese color. 
+Nota: pensar si ya existe una operación sobre 
+listas que ayude a resolver el problema.-}
 
-esColor :: Color -> Color -> Bool
-esColor Azul Azul = True
-esColor Rojo Rojo = True
-esColor _    _    = False
+nroBolitas :: Color -> Celda -> Int
+nroBolitas _ CeldaVacia = 0
+nroBolitas c (Bolita c2 celda) =
+    unoSiCeroSino (esMismoColor c c2) + nroBolitas c celda
+
+unoSiCeroSino :: Bool -> Int
+unoSiCeroSino True = 1
+unoSiCeroSino _ = 0
+
+esMismoColor :: Color -> Color -> Bool
+esMismoColor Azul Azul = True
+esMismoColor Rojo Rojo = True
+esMismoColor _ _ = False
+
+
+{-3.1.2 Dado un color y una celda, agrega una bolita
+de dicho color a la celda.-}
 
 poner :: Color -> Celda -> Celda
-poner c (CeldaVacia)     = Bolita c CeldaVacia
-poner c (Bolita col cel) = Bolita col (poner c cel)
+poner c celda = (Bolita c celda)
+
+{-3.1.3 Dado un color y una celda, quita una bolita
+de dicho color de la celda. 
+Nota: a diferencia de Gobstones, esta función es total-}
 
 sacar :: Color -> Celda -> Celda
-sacar c (CeldaVacia)     = CeldaVacia
-sacar c (Bolita col cel) = 
-  if esColor c col
-    then cel
-    else Bolita col (sacar c cel)
+sacar _ CeldaVacia = CeldaVacia
+sacar c (Bolita c2 celda) = 
+    if esMismoColor c c2
+        then celda
+        else Bolita c2 (sacar c celda)
 
-ponerN :: Int -> Color -> Celda -> Celda 
-ponerN 0 c CeldaVacia       = CeldaVacia
-ponerN n c CeldaVacia       = ponerN (n-1) c (poner c CeldaVacia)
-ponerN n c (Bolita col cel) = Bolita col (ponerN n c cel) 
+{-3.1.4 Dado un número n, un color c, y una celda, 
+agrega n bolitas de color c a la celda.-}
 
------------------------------------------------------------------------------
+ponerN :: Int -> Color -> Celda -> Celda
+ponerN 0 _ celda = celda
+ponerN n c celda = Bolita c (ponerN (n-1) c celda)
+
 data Objeto = Cacharro | Tesoro
-data Camino = Fin 
-            | Cofre [Objeto] Camino 
-            | Nada Camino
+    deriving Show
+data Camino = Fin | Cofre [Objeto] Camino | Nada Camino
+    deriving Show
 
-camino :: Camino
-camino = Nada 
-          (Cofre [Cacharro] 
-            (Cofre [Cacharro, Tesoro] 
-              Fin))
+caminoAlTesoro = Nada (Cofre [Cacharro, Tesoro] (Nada (Cofre [Tesoro, Cacharro] Fin)))
+caminoAlNoTesoro = Nada (Cofre [Cacharro, Cacharro] (Nada (Cofre [Cacharro] Fin)))
+
+caminoConMuchosTesoros = Nada (Cofre [Tesoro, Tesoro,Tesoro] (Nada (Cofre [Tesoro, Tesoro] Fin)))
+
+{-1.2.1 Indica si hay un cofre con un tesoro en el camino.-}
 
 hayTesoro :: Camino -> Bool
-hayTesoro Fin           = False
-hayTesoro (Nada c)      = hayTesoro c
-hayTesoro (Cofre obs c) = tieneTesoro obs || hayTesoro c
+hayTesoro Fin = False
+hayTesoro (Nada c) = hayTesoro c
+hayTesoro (Cofre os c) = hayTesoroAca os || hayTesoro c
 
-tieneTesoro :: [Objeto] -> Bool
-tieneTesoro []     = False
-tieneTesoro (x:xs) = esTesoro x || tieneTesoro xs
+hayTesoroAca :: [Objeto] -> Bool
+hayTesoroAca [] = False
+hayTesoroAca (o : os) = esTesoro o || hayTesoroAca os
 
 esTesoro :: Objeto -> Bool
 esTesoro Tesoro = True
-esTesoro _      = False
+esTesoro _ = False
+
+{-1.2.2 Indica la cantidad de pasos que hay que recorrer hasta
+llegar al primer cofre con un tesoro.
+Si un cofre con un tesoro está al principio del camino, 
+la cantidad de pasos a recorrer es 0.
+Precondición: tiene que haber al menos un tesoro.-}
 
 pasosHastaTesoro :: Camino -> Int
-pasosHastaTesoro (Nada c)      = 1 + pasosHastaTesoro c
-pasosHastaTesoro (Cofre obs c) = 
-  if tieneTesoro obs 
-    then 0
-    else 1 + pasosHastaTesoro c
+pasosHastaTesoro (Nada c) = 1 + pasosHastaTesoro c
+pasosHastaTesoro (Cofre os c) = 
+    if hayTesoroAca os
+        then 0
+        else 1 + pasosHastaTesoro c
+
+{-1.2.3 Indica si hay un tesoro en una cierta cantidad exacta 
+de pasos. Por ejemplo, si el número de
+pasos es 5, indica si hay un tesoro en 5 pasos.-}
 
 hayTesoroEn :: Int -> Camino -> Bool
-hayTesoroEn n Fin           = False
-hayTesoroEn 0 (Nada c)      = False
-hayTesoroEn 0 (Cofre obs c) = tieneTesoro obs
-hayTesoroEn n (Nada c)      = hayTesoroEn (n-1) c
-hayTesoroEn n (Cofre obs c) = hayTesoroEn (n-1) c
+hayTesoroEn _ Fin = False
+hayTesoroEn n (Nada c) = hayTesoroEn (n-1) c
+hayTesoroEn n (Cofre os c) = 
+    (n == 0 && hayTesoroAca os) || hayTesoroEn (n-1) c
+
+{-1.2.4 Indica si hay al menos ntesoros en el camino-}
 
 alMenosNTesoros :: Int -> Camino -> Bool
-alMenosNTesoros n c = cantidadDeTesoros c >= n
+alMenosNTesoros n c = n <= (cantidadDeTesoros c)
 
--- Considerando que solo puede haber un tesoro adentro de un cofre.
 cantidadDeTesoros :: Camino -> Int
-cantidadDeTesoros Fin           = 0
-cantidadDeTesoros (Nada c)      = cantidadDeTesoros c
-cantidadDeTesoros (Cofre obs c) = 
-  if tieneTesoro obs
-    then 1 + cantidadDeTesoros c
-    else cantidadDeTesoros c
+cantidadDeTesoros Fin = 0
+cantidadDeTesoros (Nada c) = cantidadDeTesoros c
+cantidadDeTesoros (Cofre os c) =  
+    cantidadDeTesorosAca os + cantidadDeTesoros c
+
+cantidadDeTesorosAca :: [Objeto] -> Int
+cantidadDeTesorosAca [] = 0
+cantidadDeTesorosAca (o : os) = 
+    unoSiCeroSino (esTesoro o) + cantidadDeTesorosAca os
+
+
+{-1.2.5 Dado un rango de pasos, indica la cantidad de tesoros 
+que hay en ese rango. Por ejemplo, si
+el rango es 3 y 5, indica la cantidad de tesoros que hay entre
+hacer 3 pasos y hacer 5. Están
+incluidos tanto 3 como 5 en el resultado.-}
 
 cantTesorosEntre :: Int -> Int -> Camino -> Int
-cantTesorosEntre n m Fin = 0
+cantTesorosEntre _ _ Fin = 0
 cantTesorosEntre 0 m camino = contarTesorosHasta m camino 
 cantTesorosEntre n m (Nada c) = cantTesorosEntre (n-1) (m-1) c
 cantTesorosEntre n m (Cofre obs c) = cantTesorosEntre (n-1) (m-1) c
@@ -93,177 +130,88 @@ contarTesorosHasta :: Int -> Camino -> Int
 contarTesorosHasta n Fin = 0
 contarTesorosHasta 0 c   = 0
 contarTesorosHasta n (Nada c) = contarTesorosHasta (n-1) c
-contarTesorosHasta n (Cofre obs c) = cantTesorosCofre obs + contarTesorosHasta (n-1) c
+contarTesorosHasta n (Cofre obs c) = cantidadDeTesorosAca obs + contarTesorosHasta (n-1) c
 
-cantTesorosCofre :: [Objeto] -> Int
-cantTesorosCofre [] = 0
-cantTesorosCofre (x:xs) = 
-  if esTesoro x
-    then 1 + cantTesorosCofre xs
-    else cantTesorosCofre xs
------------------------------------------------------------------------------
-data Tree a = EmptyT 
-            | NodeT a (Tree a) (Tree a) deriving Show
+-----------------------------------------------------
+
+data Tree a = EmptyT | NodeT a (Tree a) (Tree a)
+    deriving Show
+
+arbol :: Tree Int
+arbol = NodeT 10 
+            (NodeT 5 
+                (NodeT 9 
+                    EmptyT 
+                    EmptyT) 
+                EmptyT) 
+            (NodeT 2 
+                EmptyT 
+                EmptyT) 
+
+
+{-2.1.1 Dado un árbol binario de enteros 
+devuelve la suma entre sus elementos-}
 
 sumarT :: Tree Int -> Int
-sumarT EmptyT          = 0
-sumarT (NodeT x ti td) = x + sumarT ti + sumarT td
+sumarT EmptyT = 0
+sizeT EmptyT = 0
+sizeT (NodeT _ tl tr) =
+    1 + sizeT tl + sizeT tr
 
-sizeT :: Tree a -> Int
-sizeT EmptyT          = 0
-sizeT (NodeT x ti td) = 1 + sizeT ti + sizeT td
+{-2.1.3 Dado un árb ol de enteros devuelve un
+árbol con el doble de cada número.-}
 
 mapDobleT :: Tree Int -> Tree Int
-mapDobleT EmptyT          = EmptyT
-mapDobleT (NodeT x ti td) = NodeT (x*2) (mapDobleT ti) (mapDobleT td)
+mapDobleT EmptyT = EmptyT
+mapDobleT (NodeT n tl tr) =
+    (NodeT (n * 2) (mapDobleT tl) (mapDobleT tr)) 
 
 perteneceT :: Eq a => a -> Tree a -> Bool
-perteneceT x (EmptyT)        = False
-perteneceT x (NodeT a ti td) = x == a || perteneceT x ti || perteneceT x td
+perteneceT _ EmptyT = False
+perteneceT e (NodeT e1 tl tr) =
+    e == e1 || (perteneceT e tl) || (perteneceT e tr)
+
+{-2.1.4 Dados un elemento e y un árbol binario devuelve
+la cantidad de elementos del árbol que son iguales a e.-}
 
 aparicionesT :: Eq a => a -> Tree a -> Int
-aparicionesT x (EmptyT)       = 0
-aparicionesT x (NodeT a ti td) = 
-  if x == a
-    then 1 + aparicionesT x ti + aparicionesT x td
-    else aparicionesT x ti + aparicionesT x td
+aparicionesT _ EmptyT = 0
+aparicionesT e (NodeT e1 tl tr) =
+    unoSiCeroSino(e == e1) + (aparicionesT e tl) + (aparicionesT e tr)
 
-toList :: Tree a -> [a]
-toList EmptyT          = []
-toList (NodeT x ti td) = x : (toList ti ++ toList td)
-
-heightT :: Tree a -> Int
-heightT EmptyT                  = 0
-heightT (NodeT a EmptyT EmptyT) = 0
-heightT (NodeT a t1 t2)         = 1 + max (heightT t1) (heightT t2)
-
-mirrorT :: Tree a -> Tree a
-mirrorT EmptyT          = EmptyT
-mirrorT (NodeT a t1 t2) = NodeT a t2 t1
+{-2.1.5 Dado un árb ol devuelve los elementos
+que se encuentran en sus hojas-}
 
 leaves :: Tree a -> [a]
-leaves EmptyT                  = []
-leaves (NodeT a EmptyT EmptyT) = [a] 
-leaves (NodeT a t1 t2)         = leaves t1 ++ leaves t2
+leaves EmptyT = []
+leaves (NodeT e EmptyT EmptyT) = [e]
+leaves (NodeT _ tl tr) = (leaves tl) ++ (leaves tr)
+
+{-2.1.7 Dado un árb ol devuelve su altura-}
+
+heightT :: Tree a -> Int
+heightT EmptyT = 0
+heightT (NodeT _ tl tr) =
+    1 + max (heightT tl) (heightT tr)
+
+{-2.1.8 Dado un árbol devuelve el árbol resultante
+de intercambiar el hijo izquierdo con el derecho,
+en cada nodo del árbol-}
+
+mirrorT :: Tree a -> Tree a
+mirrorT EmptyT = EmptyT
+mirrorT (NodeT e tl tr) =
+    (NodeT e (mirrorT tr) (mirrorT tl))
+
+toList :: Tree a -> [a]
+toList EmptyT = []
+toList (NodeT e tl tr) =
+    toList tl ++ [e] ++ toList tr
 
 levelN :: Int -> Tree a -> [a]
-levelN n EmptyT          = []
-levelN 0 (NodeT a t1 t2) = [a]
-levelN n (NodeT a t1 t2) = levelN (n-1) t1 ++ levelN (n-1) t2 
+levelN _ EmptyT = []
+levelN 0 (NodeT e _ _) = [e]
+levelN n (NodeT e tl tr) =
+    levelN (n-1) tl ++ levelN (n-1) tr
 
-listPerLevel :: Tree a -> [[a]]
-listPerLevel EmptyT          = []
-listPerLevel (NodeT a t1 t2) = [a] : unirPerLevel (listPerLevel t1) (listPerLevel t2)
 
-unirPerLevel :: [[a]] -> [[a]] -> [[a]]
-unirPerLevel [] ys         = ys
-unirPerLevel xs []         = xs
-unirPerLevel (x:xs) (y:ys) = (x ++ y) : unirPerLevel xs ys
-
-ramaMasLarga :: Tree a -> [a]
-ramaMasLarga EmptyT          = []
-ramaMasLarga (NodeT a t1 t2) = a : ramaMasLarga (masLargoEntre t1 t2)
-
-masLargoEntre :: Tree a -> Tree a -> Tree a
-masLargoEntre EmptyT t = t
-masLargoEntre t EmptyT = t
-masLargoEntre t1 t2    = 
-  if heightT t1 > heightT t2
-    then t1
-    else t2 
-
-todosLosCaminos :: Tree a -> [[a]]
-todosLosCaminos EmptyT = []
-todosLosCaminos (NodeT x EmptyT EmptyT) = [[x]]
-todosLosCaminos (NodeT a t1 t2) = agregarATodos a (todosLosCaminos t1 ++ todosLosCaminos t2)
-
-agregarATodos :: a -> [[a]] -> [[a]]
-agregarATodos x [] = []
-agregarATodos x (xs:xss) = (x : xs) : (agregarATodos x xss)
-----------------------------------------------------------------------------------
-data ExpL = Valor Bool
-          | And   ExpL ExpL
-          | Or    ExpL ExpL
-          | Not   ExpL
-          deriving Show
-
-exp1 :: ExpL
-exp1 = And (Valor True) (Valor False)
-
-exp2 :: ExpL
-exp2 = Or exp1 (Valor True)
-
-evalL :: ExpL -> Bool
-evalL (Valor b)   = b
-evalL (And e1 e2) = evalL e1 && evalL e2
-evalL (Or e1 e2)  = evalL e1 || evalL e2
-evalL (Not e1)    = not (evalL e1)
-
-simplificarL :: ExpL -> ExpL
-simplificarL (And e1 e2) = simplAnd e1 e2
-simplificarL (Or e1 e2)  = simplOr  e1 e2
-simplificarL (Not e1)    = simplNot e1 
-simplificarL x           = x 
-
-simplAnd :: ExpL -> ExpL -> ExpL
-simplAnd (Valor True) x  = x
-simplAnd x (Valor True)  = x
-simplAnd (Valor False) x = Valor False
-simplAnd x (Valor False) = Valor False
-simplAnd x y             = And x y
-
-simplOr :: ExpL -> ExpL -> ExpL
-simplOr (Valor True) x  = Valor True
-simplOr x (Valor True)  = Valor True
-simplOr (Valor False) x = x
-simplOr x (Valor False) = x
-simplOr x y             = Or x y
-
-simplNot :: ExpL -> ExpL
-simplNot (Not e1) = e1
-simplNot x        = x
-
-esNot :: ExpL -> Bool
-esNot (Not e1) = True
-esNot _        = False
-
-t1 :: Tree Int
-t1 = NodeT 32
-        (NodeT 35
-          (NodeT 100
-              (NodeT 15
-                  EmptyT
-                  EmptyT)
-              EmptyT)
-          EmptyT)
-        (NodeT 40
-          (NodeT 50
-              EmptyT
-              EmptyT)
-          EmptyT)
-
-t2 :: Tree Int
-t2 = NodeT 1 (NodeT 2 (NodeT 9 EmptyT EmptyT) (NodeT 3 EmptyT EmptyT)) (NodeT 5 EmptyT EmptyT)
-
-pad :: [[Char]] -> [[Char]]
-pad = zipWith (++) (repeat "   ")
-
-arreglarT :: Show a => Tree a -> IO()
-arreglarT EmptyT = putStrLn "-"
-
-arreglarT tree = printRecursive (prettyprint_helper tree)
-  where 
-    printRecursive [] = putStrLn ""
-    printRecursive (x:xs) = do
-      putStrLn x
-      printRecursive xs
-
-prettyprint_helper (NodeT x left right)
-  = (show x) : (prettyprint_subtree left right)
-    where
-      prettyprint_subtree left right =
-        ((pad "+- " "|  ") (prettyprint_helper right))
-        ++ ((pad "`- " "   ") (prettyprint_helper left))
-      pad first rest = zipWith (++) (first : repeat rest)
-
-prettyprint_helper EmptyT = [""]
