9fc5429080bde0bf56f415dce7fbf5646c5019b0
ezeagusibarra001 <ibarra.ezequiel01@gmail.com>
2023-10-02 00:24:06 -0300

    P6

--- tp-6/UserM.hs
+++ tp-6/UserM.hs
@@ -7,7 +7,9 @@ import Map
 -- keys :: Eq k => Map k v -> [k]
 
 maps = assocM "a" 1 (assocM "b" 2 (assocM "c" 3 (assocM "d" 4 (assocM "e" 5 emptyM))))
+list = ["a", "b", "c", "d", "e", "deee"]
 
+listM =[("a", 1), ("b", 2), ("c", 3), ("d", 41)]
 
 valuesM :: Eq k => Map k v -> [Maybe v]
 valuesM m = maybeValores (keys m) m 
@@ -28,3 +30,21 @@ obtenerValores (k:ks) m =
 valor :: Maybe v -> v
 valor Nothing = error "no se obtener un valor"
 valor (Just x) = x
+
+todasAsociadas :: Eq k => [k] -> Map k v -> Bool
+todasAsociadas ks mp = todasLasClaves ks (keys mp)
+
+todasLasClaves :: Eq k => [k] -> [k] -> Bool
+todasLasClaves [] kvs = True
+todasLasClaves (k : ks) kvs = elem k kvs && todasLasClaves ks kvs
+
+listToMap :: Eq k => [(k, v)] -> Map k v
+listToMap [] = emptyM
+listToMap ((k,v) : kvs) = assocM k v (listToMap kvs)
+
+mapToList :: Eq k => Map k v -> [(k, v)]
+mapToList map = mapList (keys map) map
+
+mapList :: Eq k => [k] -> Map k v -> [(k, v)]
+mapList [] mp = []
+mapList (k : ks) mp = (k, valor (lookupM k mp)) : mapList ks mp
