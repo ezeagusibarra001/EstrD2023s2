eafcc141f4c68ee54095707613155c20ed91ed4f
ezeagusibarra001 <ibarra.ezequiel01@gmail.com>
2023-10-14 13:23:54 -0300

    ready

--- parciales/Nave.hs
+++ parciales/Nave.hs
@@ -17,7 +17,7 @@ data Nave = N (Map SectorId Sector) (Map Nombre Tripulante) (MaxHeap Tripulante)
 -}
 
 -- Propósito: Construye una nave con sectores vacíos, en base a una lista de identificadores de sectores.
--- Eficiencia: O(S)
+-- Eficiencia: O(S Log (S))
 construir :: [SectorId] -> Nave
 construir  ids =  N (mapaDeSectores ids) emptyM emptyH
 
@@ -30,11 +30,96 @@ mapaDeSectores (id : ids) = assocM id (crearS id) (mapaDeSectores ids)
 -- Propósito: Incorpora un tripulante a la nave, sin asignarle un sector.
 -- Eficiencia: O(log T)
 ingresarT :: Nombre -> Rango -> Nave -> Nave
-ingresarT nt rt (N s t r) = N s (agregarT nt rt t) r
+ingresarT nt rt (N s t r) = N s (agregarTM nt rt t) (agregarTH nt rt r) 
 
 --Eficiencia: Log(T)
-agregarT :: Nombre -> Rango -> (Map Nombre Tripulante) -> (Map Nombre Tripulante)
+agregarTM :: Nombre -> Rango -> (Map Nombre Tripulante) -> (Map Nombre Tripulante)
                         -- k ----v------map Log(K)
-agregarT nt rt t = assocM nt (crearT nt rt) t
+agregarTM nt rt t = assocM nt (crearT nt rt) t
 
---tripulanteN: O(T Log(T))
+agregarTH :: Nombre -> Rango -> (MaxHeap Tripulante) -> (MaxHeap Tripulante)
+                    --O(Log M)
+agregarTH nt rt r = insertH (crearT nt rt) r
+
+-- Propósito: Devuelve los sectores asignados a un tripulante.
+-- Precondición: Existe un tripulante con dicho nombre.
+-- Eficiencia: O(log M)
+sectoresAsignados :: Nombre -> Nave -> Set SectorId
+sectoresAsignados n (N mapS mapT maxHeap r) = sectoresDe n mapT
+
+sectoresDe :: Nombre -> (Map Nombre Tripulante) -> Set SectorId
+                            -- log k
+sectorDe n mapT = let t in lookupM n mapT
+                    --O(1)
+                  sectoresT (fromJust t)
+                
+isNothing :: Maybe a -> Bool
+isNothing Nothing = True
+isNothing _ = False
+
+fromJust :: Maybe v -> v
+-- precond: v no es Nothng
+fromJust (Just v) = v
+
+
+-- Propósito: Dado un sector, devuelve los tripulantes y los componentes asignados a ese sector.
+-- Precondición: Existe un sector con dicho id.
+-- Eficiencia: O(log S)
+datosDeSector :: SectorId -> Nave -> (Set Nombre, [Componente])
+                                                            -- O(1)    O(Log S)
+datosDeSector id (N mapS mapT maxHeapR) = datosDelSectorId (fromJust (lookupM id mapS))
+
+--O(1)
+datosDelSectorId :: Sector -> (Set Nombre, [Componente])
+                        -- O(1)         O(1)
+datosDelSectorId s = ((tripulantesS s), (componentesS s))
+
+
+-- Propósito: Devuelve la lista de tripulantes ordenada por rango, de mayor a menor.
+-- Eficiencia: O(T Log(T))
+ tripulantesN :: Nave -> [Tripulante]
+ tripulantesN (N mapS mapT maxHeapR) = tripulantesH maxHeapR
+
+tripulantesH :: (MaxHeap Tripulante) -> [Tripulante]
+tripulantesH maxHeapR =
+            if (isEmptyH maxHeapR)
+                then []
+                else (maxH maxHeapR) : (deleteMaxH tripulantesH)
+
+-- Propósito: Asigna una lista de componentes a un sector de la nave.
+-- Precondicion: Existe ese sectorId en la nave
+-- Eficiencia: O(C + log S), siendo C la cantidad de componentes dados.
+agregarASector :: [Componente] -> SectorId -> Nave -> Nave
+agregarASector ls id (N mapS mapT maxHeapR) = N (agregarAMapS ls id mapS) mapT maxHeapR
+
+--O(log S)
+agregarAMapS ::  [Componente] -> SectorId -> (Map SectorId Sector) -> (Map SectorId Sector)
+                            --                      log k               log k
+agregarAMapS ls id mapS = agregarAS ls (fromJust(lookupM id mapS)) (deleteM id mapS)
+
+agregarAS ::[Componente] -> Sector -> (Map SectorId Sector) -> (Map SectorId Sector)
+agregarAS [] s mapS       = assocM (sectorId s) s mapS 
+agregarAS (c : cs) s mapS = agregarAS cs (agregarC c s) mapS
+
+-- Propósito: Asigna un sector a un tripulante.
+-- Nota: No importa si el tripulante ya tiene asignado dicho sector.
+-- Precondición: El tripulante y el sector existen.
+-- Eficiencia: O(log S + log T + T log T)
+asignarASector :: Nombre -> SectorId -> Nave -> Nave
+asignarASector n id (N s t r) = N s (asignarAT n id t) (asignarAR n id r)
+
+asignarAT :: Nombre -> SectorId -> (Map Nombre Tripulante) -> (Map Nombre Tripulante)
+asignarAT n id mapT = let tripulante in fromJust(lookupM n mapT)
+                      assocM n (asignarS id tripulante) mapT
+
+asignarAR :: Nombre -> SectorId -> (MaxHeap Tripulante) -> (MaxHeap Tripulante)
+asignarAR n id maxHeapR =
+        if (isEmptyH maxHeapR)
+            then emptyH
+            else insertH (asignarATR n id (maxH maxHeapR)) (asignarAR n id (deleteMaxH maxHeapR))
+
+asignarATR :: Nombre -> SectorId -> Tripulante -> Tripulante
+asignarATR n id t =
+    if (nombre t) === n
+        then asignarS id t
+        else t
